2555c6390f9bd14adf57b0e3c8a010819225cab6
Chaeduk <kkj2551@naver.com>
2021-08-01 10:03:45 +0900

    see the token

--- Project/app/src/main/java/com/summer/project/MainActivity.kt
+++ Project/app/src/main/java/com/summer/project/MainActivity.kt
@@ -2,14 +2,17 @@ package com.summer.project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,40 +23,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
             .requestEmail()
             .build()
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-        if (account != null && account.id != null) {
-
-        }
 
         google_login.setOnClickListener {
             signIn()
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null && account.id != null) {
+
+        }
+
+    }
+
     private fun signIn():Unit{
-        val signInIntent : Intent = mGoogleSignInClient.signInIntent
+        val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         if(requestCode == RC_SIGN_IN){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
+            try{
+                val account = task.getResult(ApiException::class.java)!!
+            }catch (e: ApiException){
 
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>){
-        try{
-            val account = completedTask.getResult((ApiException::class.java))
-        } catch (e:ApiException){
-
+            }
         }
     }
 }
