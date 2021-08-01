8b11a4990a7f3f2afe91524e2eab843f169d7891
Chaeduk <kkj2551@naver.com>
2021-08-01 12:51:44 +0900

    make server

---
+++ Back-end/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const indexRouter = require('./routes')
+
+const app = express()
+
+app.set('port', process.env.PORT || 8001)
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', indexRouter)
+
+app.listen(app.get('port'), ()=>{
+  console.log(app.get('port'),'번 포트에서 대기 중')
+})
